fec5692352b737c04858ee28dd5bd7282ee3df60
juliolrmonteiro <julio.lrmonteiro@gmail.com>
2016-06-29 04:24:19 +0000

    Added fonts and initial interface!

---
+++ client/configs/page_setup.js
@@ -0,0 +1,14 @@
+/* global DocHead */
+var title = "Equilibrium";
+DocHead.setTitle(title);
+
+var metaInfo = {name: "description", content: "Equilibrium Social Network"};
+DocHead.addMeta(metaInfo);
+
+var linkInfo = {rel: "stylesheet", href: "https://fonts.googleapis.com/icon?family=Material+Icons"};
+
+DocHead.addLink(linkInfo);
+
+var linkInfo2 = {rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Roboto:400,300,500"};
+
+DocHead.addLink(linkInfo2);
--- client/modules/core/components/home.jsx
+++ client/modules/core/components/home.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import MenuItem from 'material-ui/MenuItem';
+import Avatar from 'material-ui/Avatar';
+import {
+  blue300,
+  indigo900,
+  orange200,
+  deepOrange300,
+  pink400,
+  purple500,
+} from 'material-ui/styles/colors';
+
+const style = {padding: 0};
 
 const Home = () => (
   <div>
-    <h1>Sample</h1>
+      <AppBar title="Equilibrium"
+        iconElementRight={
+          <IconMenu
+            iconButtonElement={
+              <IconButton style={style}>
+                <Avatar
+                  color={deepOrange300}
+                  backgroundColor={purple500}
+                  size={40}
+                >
+                A
+                </Avatar>
+              </IconButton>
+            }
+          targetOrigin={{horizontal: 'right', vertical: 'top'}}
+          anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+        >
+          <MenuItem primaryText="Refresh" />
+          <MenuItem primaryText="Help" />
+          <MenuItem primaryText="Sign out" />
+        </IconMenu>
+      }
+    />
+        <Drawer width={200} open={false} >
+          <MenuItem>Menu Item</MenuItem>
+        </Drawer>
     <p>
-      Welcome to Mantra 0.2.0. <RaisedButton label="Default" />
+      Welcome to Mantra 0.2.0. <RaisedButton label="Test Button" />
     </p>
     <ul>
       <li>
